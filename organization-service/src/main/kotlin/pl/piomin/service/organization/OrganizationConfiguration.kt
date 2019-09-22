cb1daf0d46787792c42b34f12c5951dced97bb19
Pratip <pratip.ibm@gmail.com>
2019-09-22 18:17:57 +0530

    ConnectionPool

--- organization-service/src/main/kotlin/pl/piomin/service/organization/OrganizationConfiguration.kt
+++ organization-service/src/main/kotlin/pl/piomin/service/organization/OrganizationConfiguration.kt
@@ -1,8 +1,9 @@
 package pl.piomin.service.organization
 
+import io.r2dbc.pool.ConnectionPool
+import io.r2dbc.pool.ConnectionPoolConfiguration
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
 import io.r2dbc.postgresql.PostgresqlConnectionFactory
-import io.r2dbc.spi.ConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.dialect.PostgresDialect
@@ -12,9 +13,31 @@ import org.springframework.data.r2dbc.repository.support.R2dbcRepositoryFactory
 import org.springframework.data.relational.core.mapping.RelationalMappingContext
 import org.springframework.web.reactive.function.client.WebClient
 import pl.piomin.service.organization.repository.OrganizationRepository
+import java.time.Duration
 
 @Configuration
 class OrganizationConfiguration {
+
+    @Bean
+    internal fun connectionPool(): ConnectionPool {
+        val connectionFactory = PostgresqlConnectionFactory(
+                PostgresqlConnectionConfiguration.builder()
+                        .host("localhost")
+                        .port(5432)
+                        .database("reactive")
+                        .username("postgres")
+                        .password("Company2019").build()
+        )
+
+        val configuration = ConnectionPoolConfiguration.builder(connectionFactory)
+                .validationQuery("SELECT 1")
+                .maxIdleTime(Duration.ofMinutes(30))
+                .initialSize(5)
+                .maxSize(20)
+                .build()
+
+        return ConnectionPool(configuration)
+    }
 
     @Bean
     fun repository(factory: R2dbcRepositoryFactory): OrganizationRepository {
@@ -29,11 +52,11 @@ class OrganizationConfiguration {
     }
 
     @Bean
-    fun databaseClient(factory: ConnectionFactory): DatabaseClient {
-        return DatabaseClient.builder().connectionFactory(factory).build()
+    fun databaseClient(connectionPool: ConnectionPool): DatabaseClient {
+        return DatabaseClient.create(connectionPool);
     }
 
-    @Bean
+    /*@Bean
     fun connectionFactory(): PostgresqlConnectionFactory {
         val config = PostgresqlConnectionConfiguration.builder() //
                 .host("localhost") //
@@ -44,7 +67,7 @@ class OrganizationConfiguration {
                 .build()
 
         return PostgresqlConnectionFactory(config)
-    }
+    }*/
 
     @Bean
     fun clientBuilder() : WebClient.Builder {
